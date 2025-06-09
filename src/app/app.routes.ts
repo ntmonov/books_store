d091629dd830094d86b9e2460f50e4a266c5f68c
Nikolay Monov <n.monov@viv-design.com>
2025-06-09 18:00:42 +0300

    Login and Header components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('../auth/pages/login/login.component').then(mod => mod.LoginComponent)
+  }
+];
---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, UserCredential } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  login(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+}
