fe0c53c4c5c399b4b513f3fd0e852165674fcf1f
Nikolay Monov <n.monov@viv-design.com>
2025-06-12 13:40:32 +0300

    Home component has add book buutton

--- src/home/pages/home/home.component.ts
+++ src/home/pages/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from '@angular/fire/auth';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private auth: AuthService) {}
 
+  getUser(): Observable<User | null> {
+    return this.auth.user$;
+  }
 }
